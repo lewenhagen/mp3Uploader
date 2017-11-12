b404e7b9b9fdb3f717d00c5b1875ac10d5b9439c
Kenneth Lewenhagen <lewenhagen@live.com>
2017-11-12 21:07:04 +0100

    done

--- verify.php
+++ verify.php
@@ -5,7 +5,7 @@ $pass = isset($_POST["pass"]) ? htmlentities($_POST["pass"]) : null;
 $hp = isset($_POST["hp"]) ? htmlentities($_POST["hp"]) : null;
 $assoc = isset($_POST["assoc"]) ? htmlentities($_POST["assoc"]) : null;
 
-if ($assoc == "not") {
+if ($hp != "human") {
     $_SESSION["flash"] = "Välj förening!";
     header("Location: index.php");
 }
